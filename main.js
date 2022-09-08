49629aef7140960634426db2b94c6f7ea0087bcf
iAmritMalviya <malviyaamrat42@gmail.com>
2022-09-08 10:27:17 +0530

    uploaded

--- main.js
+++ main.js
@@ -5,27 +5,38 @@ var express = require("express");
 (mongoose = require("mongoose")),
   (ejs = require("ejs")),
   (bodyParser = require("body-parser"));
+const fs = require("fs");
 const multer = require("multer");
+const path = require("path");
+var router = express.Router();
 
-var fs = require("fs");
-var path = require("path");
 // const here
 const app = express();
+app.use(bodyParser.json());
 
 // app use set here
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(express.static("public"));
-app.use(express.static('files'))
+app.use(express.static("files"));
 
 // const Comment = require('./models/commets');
 
 const db = require("./models/blogdb");
-const { countBy } = require("lodash");
 const Blog = db.Blog;
 const Comment = db.Comment;
 let blogs;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+var upload = multer({ storage: storage });
 
 Blog.find({}, function (err, data) {
   if (err) {
@@ -36,15 +47,6 @@ Blog.find({}, function (err, data) {
   }
 });
 
-const storage = multer.diskStorage({
-  destination: "./files/images",
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage }).single('image');
-
 // routes here
 app.get("/", function (req, res) {
   Blog.find({}, function (err, data) {
@@ -53,89 +55,114 @@ app.get("/", function (req, res) {
       res.status(500).send("An error occurred", err);
     } else {
       blogs = data;
-      console.log(blogs);
 
       res.render("home", { title: "HOME", data: data });
     }
   });
 });
 
-app
-  .route("/compose")
-  .get((req, res) => {
-    res.render("compose", { title: "COMPOSE" });
-  })
-  .post((req, res) => {
-    upload(req, res, (err) => {
-      
-      if (err) {
-        console.log(err);
-      } else {
-        const newImage = new Blog({
-          title: req.body.title,
-          content: req.body.content,
-          // img: {
-          //   data: req.file.filename,
-          //   contentType: "image/png",
-          // },
-         
-        });
-        
-        newImage
-          .save()
-          .then(() => {
-            console.log("successs");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    });
+app.get("/compose", (req, res) => {
+  res.render("compose", { title: "COMPOSE" });
+});
 
-    res.redirect("/");
-  });
+app.post("/compose", upload.single("image"), (req, res) => {
+  var obj = {
+    title: req.body.title,
+    content: req.body.content,
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+
+  let blog = Blog(obj);
+  blog.save();
+
+  res.redirect("back");
+});
 
 app
   .route("/posts/:topic")
-  .get((req, res) => {
+  .get(async (req, res) => {
     let searchTitle = req.params.topic;
+    let commentfiles = [];
 
-    blogs.forEach((element) => {
-      let postTitle = element.title;
-      if (searchTitle == postTitle) {
-        console.log("match found");
-        res.render("post", { title: postTitle, content: element.content });
+    Blog.find({ title: searchTitle })
+    .lean()
+      .populate("comment")
+      .exec((err, docs) => {
+        if (err) {
+          console.log(err);
+        } else {
+
+          // console.log(docs);
+          let comment = docs;
+          //  console.log(comment[0].comment);
+
+          if (comment === undefined) {
+            
+            // comment[0].comment.forEach((element) => {
+            //   commentfiles.push(element.comment);
+              console.log('this is undefined');
+            // });
+          }
+        }
+      });
+    Blog.find({}, function (err, data) {
+      if (err) {
+
+        res.status(500).send('An error occurred', err);
+      } else {
+        data.forEach((element) => {
+          let postTitle = element.title;
+          if (searchTitle == postTitle) {
+            setTimeout(() => {
+
+              res.render("post", {
+                title: postTitle,
+                content: element.content,
+                // comment: commentfiles || '',
+                image: element.img,
+                id: element._id,
+              });
+            }, 1000);
+          }
+        });
       }
     });
   })
-  .post((req, res) => {
-    deleteTitle = req.body.title;
-    console.log(deleteTitle);
 
-  if (deleteTitle != undefined) {
-    Blog.deleteOne({ title: deleteTitle })
-    .then(function () {
-      console.log("blog has been deleted");
-      res.redirect("/");
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-    
-  }
-  else
-  {
-    res.redirect('back')
-    let comments = req.body.comment;
-    let newComment = Comment({
-      comment: comments,
-    })
-    newComment.save(() =>{
-      console.log('comment has been saved');
-      
-    })
-  }
+  .post(async (req, res) => {
+    deleteTitle = req.body.delete;
+    if (deleteTitle != undefined) {
+      Blog.deleteOne({ title: deleteTitle })
+        .then(function () {
+          console.log("blog has been deleted");
+          res.redirect("/");
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    } else {
+      const comment = new Comment({ comment: req.body.comment });
+      await comment.save();
+      await Blog.findOneAndUpdate(
+        { _id: req.body._id },
+        { $push: { comment } }
+      );
+
+      res.redirect("back");
+    }
   });
+
+//   router.post("/do-comment", async (req,res) =>{
+//     const comment = new Comment({name:req.body.name, email:req.body.email, comment:req.body.comment});
+//     await comment.save();
+//     await Blog.findOneAndUpdate({_id:req.body._id}, {$push: {comment}});
+//     res.send("Comment was added successfully");
+// })
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
--- models/blogdb.js
+++ models/blogdb.js
@@ -1,36 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // mongoose.connect("mongodb+srv://test123:test123@cluster0.m0y1zik.mongodb.net/blogdb");
 
-mongoose.connect('mongodb://localhost:27017/newDB')
-
-
-const Comment = mongoose.model(
-  "Comment",
-  new mongoose.Schema({
-    comment: String,
-  })
+mongoose.connect("mongodb://localhost:27017/newDB",  { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+  console.log('connected')
+});
+var commentSchema = new mongoose.Schema(
+  {
+    comment: {
+      type: String,
+    },
+    blog: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-  exports.Comment = Comment;
-  
-  const blogSchema = new mongoose.Schema({
-      title: {
-        type: String,
-        unique: true
-      },
-      content: String, 
+const Comment = mongoose.model("Comment", commentSchema);
 
-      img: 
-      {
-        data: Buffer,
-        contentType: String
-      },
-      comments: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Comment"
-        }
-      ]
-    })
-    
-    exports.Blog = mongoose.model('blog', blogSchema)
+exports.Comment = Comment;
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    // required: true,
+  },
+
+  content: String,
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+
+  comment: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+const Blog = mongoose.model("Blog", blogSchema);
+
+exports.Blog = Blog;
